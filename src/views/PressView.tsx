8f9d11029c0e521646968b46d70618b9c7163a9d
davidpaz06 <davidalbertopaz@gmail.com>
2025-08-18 03:20:48 -0400

    feat: add PressView and SponsorsView components with routing support

---
+++ src/views/PressView.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const PressView: FC = () => {
+  return (
+    <div>
+      <h1>PressView Component</h1>
+    </div>
+  );
+};
+
+export default PressView;
---
+++ src/views/SponsorsView.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const SponsorsView: FC = () => {
+  return (
+    <div>
+      <h1>SponsorsView Component</h1>
+    </div>
+  );
+};
+
+export default SponsorsView;
